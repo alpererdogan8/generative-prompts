4f0c0004cbc00fc0a0eb5c73048cc2a40860f7db
alpererdogan8 <alpererdogan8@gmail.com>
2023-04-14 11:54:14 +0300

    create tabs url is changed

--- src/background/background.js
+++ src/background/background.js
@@ -2,7 +2,7 @@ let manifest_version = chrome.runtime.getManifest().manifest_version;
 
 const openChatGpt = () => {
 	chrome.tabs.create({
-		url: 'https://chat.openai.com/chat',
+		url: 'https://chat.openai.com/',
 	});
 };
 
