57bbf3dc974de4d666549b3a678cd841eb664067
Alper Erdogan <alpererdogan8@gmail.com>
2024-05-24 12:19:53 +0300

    refactor: Improve code readability and optimize DOM manipulation in index.js

--- src/content_scripts/main.jsx
+++ src/content_scripts/main.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 import PromptsContextProvider from './context/Context_Api';
+import './index.css';
 
 const rootEl = document.querySelector('div[id="__next"]');
 
 //The created button is being added to the first index of the selected tag
 const createHtmlSpan = () => {
 	const nav = document.querySelector('nav');
+	const header = document.querySelector(
+		'#__next > div.relative.z-0.flex.h-full.w-full.overflow-hidden > div.flex-shrink-0.overflow-x-hidden.bg-token-sidebar-surface-primary > div > div > div > div > nav > div.flex.justify-between.flex.h-14.items-center',
+	);
 
 	const button = document.createElement('span');
 	button.id = 'generative_prompts';
-	button.className = 'flex items-center -mb-1 mt-2.5 w-full';
-
-	nav.prepend(button);
+	nav.insertBefore(button, header.nextSibling);
 
 	return ReactDOM.createRoot(document.getElementById('generative_prompts')).render(
 		<PromptsContextProvider>
